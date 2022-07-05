bc45d5518399d0a5f62f525566c1cf535ad25584
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-06 00:15:46 +0300

    Сохранили задачу №2 к ДЗ Семинара 1

---
+++ Example005DZ1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Input first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 < n2)
+{
+    Console.WriteLine(n1 + " " + "min");
+    Console.WriteLine(n2 + " " + "max");
+}
+else
+{
+        Console.WriteLine(n2 + " " + "min");
+        Console.WriteLine(n1 + " " + "max");
+}
