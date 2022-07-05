47e3b70e36f74d8f496700d50a04c7b69859f589
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-05 23:32:21 +0300

    Сохранили задачу №3

---
+++ Example003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывести последнюю цифру трехзначного числа
+
+int num, result;
+
+Console.Write("Введите 3-значное число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+result = num % 10;
+
+Console.WriteLine(result);
