8431e14d79de82629f041cd436cdfe62dfc115e7
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-05 23:54:46 +0300

    Сохранили задачу №4

---
+++ Example004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу, где на входе мы получаем число, а на выходе выводится все целые числа в промежутке от эквивалентного ему отрицательного до положительного
+
+Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = n * (-1);
+
+while(current <= n)
+{
+    Console.Write(current + " ");
+    current++; // либо current = current + 1
+}
