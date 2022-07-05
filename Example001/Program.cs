1e5b7026fa6f3f0735041a99d93546d994e1823e
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-05 22:12:30 +0300

    Сохранили задачу №1

---
+++ Example001/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1. Проверить соответствует ли число n1 квадрату числа n2
+
+int n1, n2, quad;
+
+Console.Write("Input first number: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+quad = n2*n2;
+
+if (quad == n1)
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("No");
+}
