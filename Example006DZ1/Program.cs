f1889d05003bbee5eb72b3685780b11a35c3b50d
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-06 00:35:44 +0300

    Сохранили задачу №4 к ДЗ Семинара 1

---
+++ Example006DZ1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Input first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input three number: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+if(n1 > n2)
+{
+    Console.WriteLine(n1);
+}
+else
+{
+    Console.WriteLine(n2);
+}
