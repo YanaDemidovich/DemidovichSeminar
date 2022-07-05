06cdbe15238e01f41793e1efc8d7aa835a6e6fea
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-05 23:03:01 +0300

    Срахранили задачу №2

---
+++ Example002/Program.cs
@@ -0,0 +1,45 @@
+﻿// Написать программу, торая будет выдавать название дней недели по заданному номеру
+
+int n1;
+
+Console.Write("Input number: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 == 1)
+{
+    Console.Write("Monday");
+}
+else
+    if (n1 == 2)
+{
+   Console.Write("Tuesday");
+}
+else
+    if (n1 == 3)
+{
+    Console.Write("Wednesday");
+}
+else
+    if (n1 == 4)
+{
+    Console.Write("Thursday");
+}
+else
+    if (n1 == 5)
+{
+    Console.Write("Friday");
+}
+else
+    if (n1 == 6)
+{
+    Console.Write("Saturday");
+}
+else
+    if (n1 == 7)
+{
+    Console.Write("Sunday");
+}
+else
+{
+    Console.Write("Error");
+}
