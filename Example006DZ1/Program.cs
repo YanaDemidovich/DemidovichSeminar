5a17927d3528cfcd414b6561709dd35e0d9a86e8
Demidovich <demyanvo1990@gmail.com>
2022-07-08 18:39:47 +0300

    Исправила задачу №4 к ДЗ Семинара 1

--- Example006DZ1/Program.cs
+++ Example006DZ1/Program.cs
@@ -9,11 +9,10 @@ int n2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input three number: ");
 int n3 = Convert.ToInt32(Console.ReadLine());
 
-if(n1 > n2)
-{
-    Console.WriteLine(n1);
-}
-else
-{
-    Console.WriteLine(n2);
-}
+int max = n1;
+
+if(n1 > max) max = n1;
+if(n2 > max) max = n2;
+if(n3 > max) max = n3;
+
+    Console.WriteLine(max);
